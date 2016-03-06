43f2e60d379213bd02ca02a2fc92cbb44eae2278
Paul Kristoff <paulkristoff@pauls-imac.attlocal.net>
2016-03-06 11:25:09 -0500

    Set mail up for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,13 +28,14 @@ Rails.application.configure do
   config.assets.debug = true
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.kristoffs.com",
     port: 587,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    user_name: 'gardening@kristoffs.com',
+    password: '0rgan1c@mp0st',
+    openssl_verify_mode: 'none'
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
