ddd0fa48e59082c8e9bb6e70360b6c10fffea65a
Paul Kristoff <paulkristoff@pauls-imac.attlocal.net>
2016-03-08 05:39:23 -0500

    get email working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,17 +28,17 @@ Rails.application.configure do
   config.assets.debug = true
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.kristoffs.com",
+    address: Rails.application.secrets.email_provider_address,
     port: 587,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: 'gardening@kristoffs.com',
-    password: '0rgan1c@mp0st',
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password,
     openssl_verify_mode: 'none'
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
