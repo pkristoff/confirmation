59e4163ba7e2cb9572ddbb3a4a74f66185d40400
Paul Kristoff <paulkristoff@pauls-imac.attlocal.net>
2016-05-17 13:08:38 -0400

    fixed edit account saving - merged code edit/new candidate - cleaned uo code

--- app/controllers/dev/registrations_controller.rb
+++ app/controllers/dev/registrations_controller.rb
@@ -25,5 +25,24 @@ module Dev
       Devise.mappings[:candidate]
     end
 
+    def configure_permitted_parameters
+      if (devise_mapping.name == :admin)
+        super
+      else
+        devise_parameter_sanitizer.for(:sign_in) do |candidate_parms|
+          candidate_parms.permit(:candidate_id, :parent_email_1)
+        end
+        devise_parameter_sanitizer.for(:sign_up) do |candidate_parms|
+          candidate_parms.permit(:candidate_id, :first_name, :last_name, :candidate_email, :parent_email_1,
+                                 :parent_email_2, :grade, :attending, :password, :password_confirmation)
+        end
+        devise_parameter_sanitizer.for(:account_update) do |candidate_parms|
+          candidate_parms.permit(:candidate_id, :first_name, :last_name, :candidate_email, :parent_email_1,
+                                 :parent_email_2, :grade, :attending, :password, :password_confirmation,
+                                 :current_password)
+        end
+      end
+    end
+
   end
 end
--- spec/views/candidates/registrations/new.html.erb_spec.rb
+++ spec/views/candidates/registrations/new.html.erb_spec.rb
@@ -1,4 +1,5 @@
 include DeviseHelpers
+include ViewsHelpers
 describe 'candidates/registrations/new.html.erb' do
   it 'Form layout' do
 
@@ -6,27 +7,7 @@ describe 'candidates/registrations/new.html.erb' do
 
     render
 
-    expect(rendered).to have_selector('form[id=new_candidate][action="/dev/candidates"]')
-
-    expect(rendered).to have_field('Candidate', type: 'text')
-
-    expect(rendered).to have_field('First name', with: '', type: 'text')
-    expect(rendered).to have_field('Last name', with: '', type: 'text')
-
-    expect(rendered).to have_field('Grade', type: 'number')
-
-    expect(rendered).to have_unchecked_field('Catholic High School', type: 'radio')
-    expect(rendered).to have_checked_field('The Way', type: 'radio')
-
-    expect(rendered).to have_field('Candidate email', with: '', type: 'email')
-    expect(rendered).to have_field('Parent email 1', with: '', type: 'email')
-    expect(rendered).to have_field('Parent email 2', with: '', type: 'email')
-
-    expect(rendered).to have_field('Password', type: 'password')
-    expect(rendered).to have_field('Password confirmation', type: 'password')
-    expect(rendered).to have_field('Current password', type: 'password')
-
-    expect(rendered).to have_button('Sign up')
+    expect_edit_and_new_view(rendered, @resource, '/dev/candidates', 'Sign up', true, true)
 
   end
 end
