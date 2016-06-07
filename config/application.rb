2041db70d20dd44bd6c7802ceabc6036372d4d18
Paul Kristoff <paulkristoff@pauls-imac.attlocal.net>
2016-06-07 15:46:34 -0400

    22: attempt to get css file loaded in production

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module Confirmation
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.serve_static_assets = true
   end
 end
