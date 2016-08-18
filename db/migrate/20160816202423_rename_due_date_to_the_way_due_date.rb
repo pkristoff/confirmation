b020cb4b71c86dce6d0edb5057594129b4790668
Paul Kristoff <paul.kristoff@kristoffs.com>
2016-08-18 19:42:15 -0400

    16: fixed bug where chs_due_date is not set for migrated confirmation_event

--- db/migrate/20160816202423_rename_due_date_to_the_way_due_date.rb
+++ db/migrate/20160816202423_rename_due_date_to_the_way_due_date.rb
@@ -2,5 +2,10 @@ class RenameDueDateToTheWayDueDate < ActiveRecord::Migration
   def change
     rename_column(:confirmation_events, :due_date, :the_way_due_date)
     add_column(:confirmation_events, :chs_due_date, :date)
+    ConfirmationEvent.all.each do |ce|
+      puts "CE: #{ce.name} ce.the_way_due_date=#{ce.the_way_due_date} ce.chs_due_date=#{ce.chs_due_date}"
+      ce.chs_due_date = ce.the_way_due_date
+      ce.save
+  end
   end
 end
