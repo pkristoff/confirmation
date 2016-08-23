654a99f2b851d334d3f2f4fb3b1a9a97bf092930
Paul Kristoff <paul.kristoff@kristoffs.com>
2016-08-23 16:15:17 -0400

    17,18: Add Sponsor Covenant & Sponsor Eligibility forms - fixed broken admin events screen

---
+++ spec/features/admins/admin_events_spec.rb
@@ -0,0 +1,41 @@
+include Warden::Test::Helpers
+Warden.test_mode!
+
+# Feature: Admin index page
+#   As a admin
+#   I want to see a list of admins
+#   So I can see who has registered
+feature 'Admin events page', :devise do
+
+  after(:each) do
+    Warden.test_reset!
+  end
+
+  scenario 'admin list of no events' do
+    admin = FactoryGirl.create(:admin)
+    login_as(admin, scope: :admin)
+    visit events_path
+
+    expect(page).to have_selector('form', count: 0)
+  end
+
+  scenario 'admin list of 1 events' do
+    AppFactory.add_confirmation_event(I18n.t('events.sign_agreement'))
+    admin = FactoryGirl.create(:admin)
+    login_as(admin, scope: :admin)
+    visit events_path
+
+    expect(page).to have_selector('form', count: 1)
+  end
+
+  scenario 'admin list of 2 events' do
+    AppFactory.add_confirmation_event(I18n.t('events.sign_agreement'))
+    AppFactory.add_confirmation_event(I18n.t('events.confirmation_name'))
+    admin = FactoryGirl.create(:admin)
+    login_as(admin, scope: :admin)
+    visit events_path
+
+    expect(page).to have_selector('form', count: 2)
+  end
+
+end
