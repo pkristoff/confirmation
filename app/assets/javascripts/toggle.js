ecda8dc59aebad8cae7afb57168285be1a769b5f
Paul Kristoff <paulkristoff@pauls-imac.attlocal.net>
2017-02-14 09:17:10 -0500

    54: Verification overrides completed - added to an individual event and event mass update

--- app/assets/javascripts/toggle.js
+++ app/assets/javascripts/toggle.js
@@ -2,28 +2,45 @@
  * Created by paulkristoff on 9/1/16.
  */
 
-select_all_none = function (id) {
-    var check_all = document.getElementById(id).checked,
-        checkboxes = document.querySelectorAll('tr[class="odd"]>td>input[type=checkbox]');
-    for (var i = 0, len = checkboxes.length; i < len; i++){
-        checkboxes[i].checked = check_all;
+update_completed_date = function ( my_id ) {
+    var comp_id       = my_id.replace( 'verified', 'completed_date' ),
+        input_element = document.getElementById( comp_id ),
+        current_value = input_element.value;
+    if ( !current_value || 0 === current_value.length ) {
+        var date  = new Date(),
+            day   = date.getDate(),
+            month = date.getMonth() + 1,
+            year  = date.getFullYear();
+
+        if ( month < 10 ) month = "0" + month;
+        if ( day < 10 ) day = "0" + day;
+
+        document.getElementById( comp_id ).value = year + "-" + month + "-" + day;
     }
-    checkboxes = document.querySelectorAll('tr[class="even"]>td>input[type=checkbox]');
-    for (i = 0, len = checkboxes.length; i < len; i++){
-        checkboxes[i].checked = check_all;
+};
+
+select_all_none = function ( id ) {
+    var check_all  = document.getElementById( id ).checked,
+        checkboxes = document.querySelectorAll( 'tr[class="odd"]>td>input[type=checkbox]' );
+    for ( var i = 0, len = checkboxes.length; i < len; i++ ) {
+        checkboxes[ i ].checked = check_all;
+    }
+    checkboxes = document.querySelectorAll( 'tr[class="even"]>td>input[type=checkbox]' );
+    for ( i = 0, len = checkboxes.length; i < len; i++ ) {
+        checkboxes[ i ].checked = check_all;
     }
     update_total_selections();
 };
 
 update_total_selections = function () {
-    var checkboxes = document.querySelectorAll('tbody>tr>td>input[type=checkbox]'),
-        count = 0;
-    for (i = 0, len = checkboxes.length; i < len; i++){
-        if (checkboxes[i].checked){
+    var checkboxes = document.querySelectorAll( 'tbody>tr>td>input[type=checkbox]' ),
+        count      = 0;
+    for ( i = 0, len = checkboxes.length; i < len; i++ ) {
+        if ( checkboxes[ i ].checked ) {
             count++;
         }
     }
-    document.querySelector('input[id=total_selected]').value = count;
+    document.querySelector( 'input[id=total_selected]' ).value = count;
 };
 
 confirmation_toggle = function () {
@@ -34,20 +51,20 @@ confirmation_toggle = function () {
         var div = $( '#instructions' );
         div.is( ':hidden' ) ? div.show() : div.hide();
         // change icon
-        var span = $( '#insturction-toggle-span' ),
+        var span      = $( '#insturction-toggle-span' ),
             right_div = $( '#right-col' ),
-            left_div = $( '#left-col' );
+            left_div  = $( '#left-col' );
         if ( div.is( ':hidden' ) ) {
             span.removeClass( 'glyphicon-minus' );
             span.addClass( 'glyphicon-plus' );
-            right_div.width('0%');
-            left_div.width('100%');
+            right_div.width( '0%' );
+            left_div.width( '100%' );
         }
         else {
             span.removeClass( 'glyphicon-plus' );
             span.addClass( 'glyphicon-minus' );
-            right_div.width('50%');
-            left_div.width('50%');
+            right_div.width( '50%' );
+            left_div.width( '50%' );
         }
     }
 
@@ -56,7 +73,7 @@ confirmation_toggle = function () {
         // toggle the sidebar
         $( "#wrapper" ).toggleClass( "toggled" );
         // change icon
-        if ( $( self ).data( 'name' ) == 'show' ) {
+        if ( $( self ).data( 'name' ) === 'show' ) {
             $( '#menu-toggle-span' ).replaceWith( '<span id="menu-toggle-span">&raquo;</span>' );
             $( self ).data( 'name', 'hide' )
         }
@@ -66,21 +83,14 @@ confirmation_toggle = function () {
         }
     }
 
-    function toggle_top(id) {
+    function toggle_top( id ) {
         var div = $( id );
         div.is( ':hidden' ) ? div.show() : div.hide();
     }
 
-    // function initialize() {
-    //     $( "menu-toggle" ).click( sidebar );
-    //     $( "insturction" ).click( instructions );
-    // }
-    //
-    // initialize();
-
     return {
-        instructions:                 instructions,
-        sidebar:                      sidebar,
-        toggle_top: toggle_top
+        instructions: instructions,
+        sidebar:      sidebar,
+        toggle_top:   toggle_top
     }
 };
