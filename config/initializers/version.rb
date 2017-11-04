cf6ed1d59849e5420bd35d3d23ace8c79d14052b
Paul Kristoff <paulkristoff@pauls-imac-2.attlocal.net>
2017-11-03 20:15:53 -0400

    85: get error when octet-stream image is scanned in

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,2 +1,2 @@
-Rails.application.config.VERSION = 'v1.0.11'
-Rails.application.config.VERSION_DATE = '10/22/2017'
+Rails.application.config.VERSION = 'v1.0.12'
+Rails.application.config.VERSION_DATE = '11/03/2017'
