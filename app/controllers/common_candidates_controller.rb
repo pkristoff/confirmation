445e5452d6d5ff898d7de59cb779c1411c43dad3
Paul Kristoff <paul.kristoff@kristoffs.com>
2017-11-03 21:01:56 -0500

    85: get error when octet-stream image is scanned in (7)

--- app/controllers/common_candidates_controller.rb
+++ app/controllers/common_candidates_controller.rb
@@ -226,8 +226,10 @@ class CommonCandidatesController < ApplicationController
 
   def send_image(scanned_image)
     conts = scanned_image.content
+    c_type = scanned_image.content_type
+    c_type = 'application/psd' if c_type === 'application/octet-stream'
     send_data conts,
-              type: scanned_image.content_type,
+              type: c_type,
               disposition: 'inline'
   end
 
