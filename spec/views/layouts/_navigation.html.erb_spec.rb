ed67a4441eef39c1fbd067ebd013596cf71eaf62
Paul Kristoff <paul.kristoff@kristoffs.com>
2018-06-19 14:47:26 -0400

    123: Add link to website for send grid mail - for admin only

--- spec/views/layouts/_navigation.html.erb_spec.rb
+++ spec/views/layouts/_navigation.html.erb_spec.rb
@@ -4,7 +4,7 @@ describe 'layouts/_navigation.html.erb' do
   it 'navigation layout' do
     render
 
-    expect_common
+    expect_common(false)
     expect(rendered).to have_link(I18n.t('views.top_bar.sign_in', name: '').strip, href: '/dev/candidates/sign_in')
     expect(rendered).to have_link(I18n.t('views.top_bar.sign_in', name: 'admin'), href: '/admins/sign_in')
   end
@@ -14,8 +14,9 @@ describe 'layouts/_navigation.html.erb' do
 
     render
 
-    expect_common
+    expect_common(true)
     expect(rendered).to have_link(I18n.t('views.top_bar.sign_out'), href: '/admins/sign_out')
+    expect(rendered).to have_link(I18n.t('views.top_bar.send_grid'), href: 'https://app.sendgrid.com')
     expect(rendered).to have_link(I18n.t('views.top_bar.candidate_checklist'), href: download_document_path(admin.id, Event::Document::CANDIDATE_CHECKLIST))
   end
 
@@ -24,16 +25,16 @@ describe 'layouts/_navigation.html.erb' do
 
     render
 
-    expect_common
+    expect_common(false)
     expect(rendered).to have_link(I18n.t('views.top_bar.sign_out'), href: '/dev/candidates/sign_out')
     expect(rendered).to have_link(I18n.t('views.top_bar.candidate_checklist'), href: dev_download_document_path(candidate.id, Event::Document::CANDIDATE_CHECKLIST))
   end
 
-  def expect_common
+  def expect_common(admin_logged_in)
     expect(rendered).to have_button(I18n.t('views.nav.toggle_navigation'))
     expect(rendered).to have_link(I18n.t('views.top_bar.home'), href: '/')
 
-    expect(rendered).to have_selector('li', count: 6)
+    expect(rendered).to have_selector('li', count: 6 + (admin_logged_in ? 1 : 0))
 
     expect(rendered).to have_link(I18n.t('views.top_bar.about'), href: '/pages/about')
     expect(rendered).to have_link(I18n.t('views.top_bar.help'), href: '#')
