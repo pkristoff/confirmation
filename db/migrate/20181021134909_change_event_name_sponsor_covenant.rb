9abecc43db7c00566b434ab68e01da95c727cbba
Paul Kristoff <paul.kristoff@kristoffs.com>
2018-10-21 16:07:33 -0400

    140: add new checklist.pdf 141: change confirmation event from 'Sponsor Covenant' to 'Sponsor Covenant & Sponsor Eligibility'

---
+++ db/migrate/20181021134909_change_event_name_sponsor_covenant.rb
@@ -0,0 +1,18 @@
+class ChangeEventNameSponsorCovenant < ActiveRecord::Migration[5.2]
+  OLD_VALUE = 'Sponsor Covenant'
+
+  def self.up
+    ce = ConfirmationEvent.find_by(name: OLD_VALUE)
+    new_value = I18n.t('events.sponsor_covenant')
+    ce.name = new_value if ce
+    ce.save if ce
+    raise(RuntimeError, "Confirmation Event not found: #{OLD_VALUE} (old value") unless ce
+  end
+  def self.down
+    new_value = I18n.t('events.sponsor_covenant')
+    ce = ConfirmationEvent.find_by(name: new_value)
+    ce.name = OLD_VALUE if ce
+    ce.save if ce
+    raise(RuntimeError, "Confirmation Event not found: #{new_value} (new value") unless ce
+  end
+end
