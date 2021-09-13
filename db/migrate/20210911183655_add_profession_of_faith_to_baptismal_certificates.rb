f649017e8cd3740a366d0e007712d43f56c4bead
Paul Kristoff <>
2021-09-13 18:01:20 -0400

    OOPS

---
+++ db/migrate/20210911183655_add_profession_of_faith_to_baptismal_certificates.rb
@@ -0,0 +1,10 @@
+class AddProfessionOfFaithToBaptismalCertificates < ActiveRecord::Migration[5.2]
+  def change
+    add_column :baptismal_certificates, :baptized_catholic, :boolean, default: false,null: false
+    add_column :baptismal_certificates, :prof_church_name, :string, default: nil
+    add_column :baptismal_certificates, :prof_date, :date, default: nil
+
+    add_reference(:baptismal_certificates, :scanned_prof, references: :scanned_images, index: true)
+    add_reference(:baptismal_certificates, :prof_church_address, references: :addresses, index: true)
+  end
+end
