42fa29a8435d3ce2c8ead081ebf73a5d9d5567a2
Paul Kristoff <>
2024-10-15 04:30:06 -0400

    Forgot commit test xlsx files

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-Rails.application.config.VERSION = 'V6.00.11'
-Rails.application.config.VERSION_DATE = '10/09/2024'
+Rails.application.config.VERSION = 'V6.00.12'
+Rails.application.config.VERSION_DATE = '10/15/2024'
